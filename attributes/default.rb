0a7b4015a3c1c3d192afab4d46c34a71de186e8c
Bartek Jarocki <bartek@smatly.com>
2018-03-24 16:45:22 +0000

    0.19.4 as a default version
    Signed-off-by: Bartek Jarocki <bartek@smatly.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['devopsdance-consul-template'].tap do |consul_template|
-  consul_template['version'] = '0.18.5'
+  consul_template['version'] = '0.19.4'
   consul_template['config_dir'] = '/etc/consul-template.d'
   consul_template['config_template_dir'] = '/etc/consul-templates'
   consul_template['config'] = {}
