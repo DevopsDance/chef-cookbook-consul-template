b81950753a31311d87739a0460e8a039c870a49f
Bartek Jarocki <bartek@smatly.com>
2019-04-17 22:44:40 +0200

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['devopsdance-consul-template'].tap do |consul_template|
-  consul_template['version'] = '0.19.5'
+  consul_template['version'] = '0.20.0'
   consul_template['config_dir'] = '/etc/consul-template.d'
   consul_template['config_template_dir'] = '/etc/consul-templates'
   consul_template['config'] = {}
